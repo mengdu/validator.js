112c572d144b2ce36c1d8fd440d3dfe32e44082c
ygl <lanyueos@qq.com>
2019-07-11 19:45:53 +0800

    添加工具函数

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
 export function isArr (val: any) {
   return Array.isArray ? Array.isArray(val) : Object.prototype.toString.call(val) === '[object Array]'
 }
+
+export function isFun (val: any) {
+  return typeof val === 'function'
+}
+
+export function isAsyncFun (val: any) {
+  return Object.prototype.toString.call(val) === '[object AsyncFunction]'
+}
