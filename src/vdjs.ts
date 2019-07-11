ffaf7c16a9f80ffea12f632116a34be7a8a293bd
ygl <lanyueos@qq.com>
2019-07-11 19:46:33 +0800

    实现验证逻辑

--- src/vdjs.ts
+++ src/vdjs.ts
@@ -1,24 +1,45 @@
-import { isArr } from './utils'
-
-interface ruleType {
-  message?: string,
-  required?: boolean,
-  validator?: () => boolean | Promise<boolean>
-}
+import { isArr, isFun } from './utils'
+import rulesFuns, { ruleType } from './rules'
 
 type rulesType = { [key: string]: ruleType | ruleType[] }
 
-function validateRule (rules: ruleType | ruleType[]) {
-  // todo
+async function validateRule (value: any, rules: ruleType | ruleType[]) {
+  if (isArr(rules)) {
+    const list = await Promise.all(rules.map((rule: ruleType) => validateRule(value, rule)))
+    return list
+  } else {
+    for (const rule in rules) {
+      if (rule !== 'message') {
+        if (!isFun(rulesFuns[rule])) throw new Error(`Rules '${rule}' not defined.`)
+        const ruleValue = (<ruleType>rules)[rule]
+
+        return {
+          [rule]: await rulesFuns[rule](value, ruleValue),
+          ruleValue: ruleValue,
+          message: (<ruleType>rules).message
+        }
+      }
+    }
+  }
 }
 
-function validate (data: {[key: string]: any}, rules: rulesType, isOne = false): Promise<any> {
-  return new Promise((resolve, reject) => {
-    const dit: { [key: string]: any } = {}
+function validate (data: { [key: string]: any }, rules: rulesType): Promise<any> {
+  return new Promise(async (resolve, reject) => {
+    const keys = Object.keys(rules)
 
-    for (let key in data) {
-      dit[key] = validateRule(data[key])
+    const runs = keys.map(key => {
+      return validateRule(data[key], rules[key])
+    })
+
+    const resultList = await Promise.all(runs)
+
+    const result: { [key: string]: any } = {}
+
+    for (const i in resultList) {
+      result[keys[i]] = resultList[i]
     }
+
+    resolve(result)
   })
 }
 
